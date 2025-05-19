5220aa32ffc2dccc7be1e9be2c64b783732c860f
Laila <175353500+lvanrooijen@users.noreply.github.com>
2025-05-19 15:45:41 +0200

    Initial commit

---
+++ backend/src/main/java/com/lvr/babab/babab/BababApplication.java
@@ -0,0 +1,13 @@
+package com.lvr.babab.babab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BababApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BababApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/lvr/babab/babab/BababApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lvr.babab.babab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BababApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
